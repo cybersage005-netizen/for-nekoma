fe154cc50c2fe9455ec16f8d1d069302642656c8
Veer <cybersage005@gmail.com>
2026-02-13 16:13:18 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/registries/BlockRegistry.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/registries/BlockRegistry.java
@@ -87,10 +87,10 @@ public class BlockRegistry {
     1. Amber Terracotta
      */
 
-    public static Block AMBER_TERRACOTTA = register("amber_terracotta", AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_YELLOW).instrument(NoteBlockInstrument.BASEDRUM)
-            .strength(1.25F).resistance(4.2F).requiresTool());
-    public static Block INDIGO_TERRACOTTA = register("indigo_terracotta", AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_BLUE).instrument(NoteBlockInstrument.BASEDRUM)
-            .strength(1.25F).resistance(4.2F).requiresTool());
+    public static final Block AMBER_TERRACOTTA = register("amber_terracotta", AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_YELLOW).instrument(NoteBlockInstrument.BASEDRUM)
+            .strength(0.90F).resistance(4.2F).requiresTool());
+    public static final Block INDIGO_TERRACOTTA = register("indigo_terracotta", AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_BLUE).instrument(NoteBlockInstrument.BASEDRUM)
+            .strength(0.90F).resistance(4.2F).requiresTool());
 
     private static Block register(String id, AbstractBlock.Settings settings) {
         return register(id, Block::new, settings);
