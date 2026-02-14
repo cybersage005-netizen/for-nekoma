d800168edd2ba3d72e44f8be990b0f3d88f23cda
cybersage005-netizen <cybersage005@gmail.com>
2026-02-14 18:56:43 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/mixin/DyeItemMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/DyeItemMixin.java
@@ -7,18 +7,14 @@ import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DyeItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
-import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static net.greenjab.nekomasfixed.util.ModColors.AMBER;
 import static net.greenjab.nekomasfixed.util.ModColors.AQUA;
 
 @Mixin(DyeItem.class)
@@ -27,11 +23,9 @@ public class DyeItemMixin {
     private void changeDye(World world, SignBlockEntity signBlockEntity, boolean front, PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
         ItemStack stack = player.getStackInHand(player.getActiveHand());
 
-        //------------------------------ AMBER DYE ------------------------------------
-        //Application of Amber dye on sign
-
-        if (stack.isOf(ItemRegistry.AMBER_DYE)) {
-            final int dyeFromMod = AMBER.getColor();
+        //------------------ AQUA DYE -------------------------------
+        if (stack.isOf(ItemRegistry.AQUA_DYE)) {
+            final int dyeFromMod = AQUA.getColor(); // get RGB color
             var signText = signBlockEntity.getText(front);
             for (int i = 0; i < 4; i++) {
                 Text line = signText.getMessage(i, false);
@@ -41,59 +35,18 @@ public class DyeItemMixin {
                 signText = signText.withMessage(i, newLine, newLine);
             }
 
-            //Chekcing for Glowing logic
+            //Updating the changes and applying it to the sign - important
+            signBlockEntity.setText(signText, front);
 
-            if (stack.isOf(Items.GLOW_INK_SAC)) {
-                for (int i = 0; i < 4; i++) {
-                    Text line = signText.getMessage(i, false);
-                    TextColor color = line.getStyle().getColor();
-                    if (color != null && color.getRgb() == AMBER.getColor()) {
-                        MutableText newLine = line.copyContentOnly();
-                        newLine.setStyle(line.getStyle().withColor(dyeFromMod).withFormatting(Formatting.BOLD));
-                        signText = signText.withMessage(i, newLine, newLine);
-                    }
-                }
-            }
-            //-----------------------------------------------------------
-            //------------------ AQUA DYE -------------------------------
+            signBlockEntity.markDirty();
+            signBlockEntity.getWorld().updateListeners(
+                    signBlockEntity.getPos(),
+                    signBlockEntity.getCachedState(),
+                    signBlockEntity.getCachedState(),
+                    Block.NOTIFY_ALL
+            );
 
-            if (stack.isOf(ItemRegistry.AQUA_DYE)) {
-                for (int i = 0; i < 4; i++) {
-                    Text line = signText.getMessage(i, false);
-                    MutableText newLine = line.copyContentOnly();
-                    newLine.setStyle(line.getStyle().withColor(AQUA.getColor()));
-
-                    signText = signText.withMessage(i, newLine, newLine);
-                }
-
-                //Chekcing for Glowing logic
-
-                if (stack.isOf(Items.GLOW_INK_SAC)) {
-                    for (int i = 0; i < 4; i++) {
-                        Text line = signText.getMessage(i, false);
-                        TextColor color = line.getStyle().getColor();
-                        if (color != null && color.getRgb() == AQUA.getColor()) {
-                            MutableText newLine = line.copyContentOnly();
-                            newLine.setStyle(line.getStyle().withColor(AQUA.getColor()).withFormatting(Formatting.BOLD));
-                            signText = signText.withMessage(i, newLine, newLine);
-                        }
-                    }
-                }
-
-
-                //Updating the changes and applying it to the sign - important
-                signBlockEntity.setText(signText, front);
-
-                signBlockEntity.markDirty();
-                signBlockEntity.getWorld().updateListeners(
-                        signBlockEntity.getPos(),
-                        signBlockEntity.getCachedState(),
-                        signBlockEntity.getCachedState(),
-                        Block.NOTIFY_ALL
-                );
-
-                cir.setReturnValue(true); // cancel vanilla dye logic
-            }
+            cir.setReturnValue(true); // cancel vanilla dye logic
         }
     }
 }
