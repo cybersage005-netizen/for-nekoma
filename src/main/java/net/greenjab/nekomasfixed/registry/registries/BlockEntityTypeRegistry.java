253c9ac5a0afb7cc662eb712ffae16e7edc4fa08
cybersage005-netizen <cybersage005@gmail.com>
2026-02-14 23:36:13 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/registries/BlockEntityTypeRegistry.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/registries/BlockEntityTypeRegistry.java
@@ -2,10 +2,7 @@ package net.greenjab.nekomasfixed.registry.registries;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.greenjab.nekomasfixed.NekomasFixed;
-import net.greenjab.nekomasfixed.registry.block.entity.ClamBlockEntity;
-import net.greenjab.nekomasfixed.registry.block.entity.ClockBlockEntity;
-import net.greenjab.nekomasfixed.registry.block.entity.EndermanHeadBlockEntity;
-import net.greenjab.nekomasfixed.registry.block.entity.NautilusBlockEntity;
+import net.greenjab.nekomasfixed.registry.block.entity.*;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -24,6 +21,8 @@ public class BlockEntityTypeRegistry {
 
     public static final BlockEntityType<EndermanHeadBlockEntity> ENDERMAN_HEAD_BLOCK_ENTITY = Registry.register(Registries.BLOCK_ENTITY_TYPE, NekomasFixed.id("enderman_head"),
             FabricBlockEntityTypeBuilder.create(EndermanHeadBlockEntity::new, BlockRegistry.ENDERMAN_HEAD, BlockRegistry.WALL_ENDERMAN_HEAD).build());
+
+
 
     public static void registerBlockEntityType() {
         System.out.println("register BlockEntityType");
