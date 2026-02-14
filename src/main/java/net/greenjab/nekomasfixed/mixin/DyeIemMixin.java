be4919eeb3afb29bbaf2e78595c2729324b3a51c
cybersage005-netizen <cybersage005@gmail.com>
2026-02-14 17:36:30 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/mixin/DyeIemMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/DyeIemMixin.java
@@ -1,15 +1,20 @@
 package net.greenjab.nekomasfixed.mixin;
 
 import net.greenjab.nekomasfixed.registry.registries.ItemRegistry;
+import net.greenjab.nekomasfixed.util.ModColors.*;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DyeItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.greenjab.nekomasfixed.util.ModColors.AMBER;
 
 @Mixin(DyeItem.class)
 public class DyeIemMixin {
@@ -17,7 +22,12 @@ public class DyeIemMixin {
     private void changeDye(World world, SignBlockEntity signBlockEntity, boolean front, PlayerEntity player, CallbackInfoReturnable<Boolean> cir){
         ItemStack stack = player.getStackInHand(player.getActiveHand());
         if(stack.isOf(ItemRegistry.AMBER_DYE)){
-            System.out.println("Yes, amber dye is used");
+            final int dyeFromMod = AMBER.getColor();
+
+            for(int i = 0; i<4; i++){
+                Text txt = signBlockEntity.getText(true).getMessage(i, false);
+                System.out.println(txt.getStyle());
+            }
         }
 
     }
---
+++ src/main/java/net/greenjab/nekomasfixed/util/ModColors.java
@@ -0,0 +1,23 @@
+package net.greenjab.nekomasfixed.util;
+
+public class ModColors {
+    // Creating new Mod colors, new colors can be added later...
+    public static final ModColors AMBER = new ModColors("amber", 0xE0AF0B);
+
+    private final String name; // Instance field
+    private final int color;   // Instance field
+
+    // Constructor
+    public ModColors(String name, int color){
+        this.name = name;
+        this.color = color;
+    }
+
+    public  String getName(){
+        return name;
+    }
+
+    public  int getColor(){
+        return color;
+    }
+}
