497e38c59ade1489f4c224acc51f0fae05468a27
cybersage005-netizen <cybersage005@gmail.com>
2026-02-15 13:34:21 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
@@ -1,9 +1,18 @@
 package net.greenjab.nekomasfixed.registry.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class SickleItem extends Item {
     public SickleItem(Settings settings) {
         super(settings);
     }
+    @Override
+    public void postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        System.out.println("Sickle used");
+
+    }
+
 }
