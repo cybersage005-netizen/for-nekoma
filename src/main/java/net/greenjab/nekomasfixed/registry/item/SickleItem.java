d7b01038c89c87f6da33431727e84857b484651e
cybersage005-netizen <cybersage005@gmail.com>
2026-02-16 00:31:02 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
@@ -1,11 +1,6 @@
 package net.greenjab.nekomasfixed.registry.item;
 
-import com.google.common.collect.Multimap;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,12 +8,15 @@ import net.minecraft.server.world.ServerWorld;
 
 public class SickleItem extends Item {
 
-    public static final float DAMAGE_IRON = 1f;
+    public static final float DAMAGE_IRON = 3f;
     public static final float SPEED = 4.0f;
 
     public SickleItem(Settings settings) {
         super(settings);
     }
+
+    long lastHitAt = 0;
+    long currentHitAt = 0;
 
     int previousEntityId;
     private int attackCount = 1;
@@ -26,15 +24,23 @@ public class SickleItem extends Item {
 
     public void postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         if (attacker instanceof PlayerEntity player) {
-
+            currentHitAt = attacker.getEntityWorld().getTime();
             boolean inMain = player.getMainHandStack().isOf(stack.getItem());
             boolean inOff  = player.getOffHandStack().isOf(stack.getItem());
             String entityTargetted = target.getEntity().getName().getString();
             int entityID = target.getEntity().getId();
             if (inMain && inOff) {
+
                 if(entityID == previousEntityId){
                     attackCount+=1;
                     previousEntityId = entityID;
+                    if(attackCount>1){
+                        lastHitAt = currentHitAt;
+                        currentHitAt = attacker.getEntityWorld().getTime();
+                        if(currentHitAt - lastHitAt > 40){
+                            attackCount = 1;
+                        }
+                    }
                     if(attackCount==5){
                         System.out.println("Now dealing extra damage of " + ((3.0*DAMAGE_IRON)+(3.0*DAMAGE_IRON*0.5)));
                         target.damage((ServerWorld) target.getEntityWorld(), player.getDamageSources().playerAttack(player), (float) ((3.0*DAMAGE_IRON)+(3.0*DAMAGE_IRON*0.5)));
