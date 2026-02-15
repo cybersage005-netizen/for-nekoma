d25985ddc579c3c2537b06d6b060c78c01153460
cybersage005-netizen <cybersage005@gmail.com>
2026-02-16 00:13:50 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 
 public class SickleItem extends Item {
 
@@ -31,14 +32,15 @@ public class SickleItem extends Item {
             String entityTargetted = target.getEntity().getName().getString();
             int entityID = target.getEntity().getId();
             if (inMain && inOff) {
-                System.out.println("Dual wield sickles combo! attacked on " + target.getEntity().getId());
                 if(entityID == previousEntityId){
                     attackCount+=1;
                     previousEntityId = entityID;
                     if(attackCount==5){
                         System.out.println("Now dealing extra damage of " + ((3.0*DAMAGE_IRON)+(3.0*DAMAGE_IRON*0.5)));
+                        target.damage((ServerWorld) target.getEntityWorld(), player.getDamageSources().playerAttack(player), (float) ((3.0*DAMAGE_IRON)+(3.0*DAMAGE_IRON*0.5)));
                     }else if(attackCount==6){
                         System.out.println("Now dealing extra damage of " + ((4.0*DAMAGE_IRON)+(4.0*DAMAGE_IRON)));
+                        target.damage((ServerWorld) target.getEntityWorld(), player.getDamageSources().playerAttack(player), (float) (2.0*4.0*DAMAGE_IRON));
                         attackCount=1;
                     }
                 }else{
@@ -46,7 +48,7 @@ public class SickleItem extends Item {
                     previousEntityId = entityID;
                 }
 
-                System.out.println("Combo at lvl: " + attackCount);
+
             } else if (inMain) {
                 System.out.println("Main hand sickle");
             }
