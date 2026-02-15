173307bd1b5eee7347dd889f8fd9694fbcb87c46
cybersage005-netizen <cybersage005@gmail.com>
2026-02-16 00:42:24 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/item/SickleItem.java
@@ -1,65 +1,65 @@
 package net.greenjab.nekomasfixed.registry.item;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 
 public class SickleItem extends Item {
 
-    public static final float DAMAGE_IRON = 3f;
+    public static final float DAMAGE_IRON = 2f;
     public static final float SPEED = 4.0f;
 
-    public SickleItem(Settings settings) {
+    public SickleItem(Item.Settings settings) {
         super(settings);
     }
 
     long lastHitAt = 0;
-    long currentHitAt = 0;
-
     int previousEntityId;
     private int attackCount = 1;
+
     @Override
-
     public void postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        if (attacker instanceof PlayerEntity player) {
-            currentHitAt = attacker.getEntityWorld().getTime();
-            boolean inMain = player.getMainHandStack().isOf(stack.getItem());
-            boolean inOff  = player.getOffHandStack().isOf(stack.getItem());
-            String entityTargetted = target.getEntity().getName().getString();
-            int entityID = target.getEntity().getId();
-            if (inMain && inOff) {
+        if (!(attacker instanceof PlayerEntity player)) return;
 
-                if(entityID == previousEntityId){
-                    attackCount+=1;
-                    previousEntityId = entityID;
-                    if(attackCount>1){
-                        lastHitAt = currentHitAt;
-                        currentHitAt = attacker.getEntityWorld().getTime();
-                        if(currentHitAt - lastHitAt > 40){
-                            attackCount = 1;
-                        }
-                    }
-                    if(attackCount==5){
-                        System.out.println("Now dealing extra damage of " + ((3.0*DAMAGE_IRON)+(3.0*DAMAGE_IRON*0.5)));
-                        target.damage((ServerWorld) target.getEntityWorld(), player.getDamageSources().playerAttack(player), (float) ((3.0*DAMAGE_IRON)+(3.0*DAMAGE_IRON*0.5)));
-                    }else if(attackCount==6){
-                        System.out.println("Now dealing extra damage of " + ((4.0*DAMAGE_IRON)+(4.0*DAMAGE_IRON)));
-                        target.damage((ServerWorld) target.getEntityWorld(), player.getDamageSources().playerAttack(player), (float) (2.0*4.0*DAMAGE_IRON));
-                        attackCount=1;
-                    }
-                }else{
-                    attackCount = 1;
-                    previousEntityId = entityID;
-                }
+        long now = attacker.getEntityWorld().getTime();
 
+        boolean inMain = player.getMainHandStack().isOf(stack.getItem());
+        boolean inOff  = player.getOffHandStack().isOf(stack.getItem());
 
-            } else if (inMain) {
-                System.out.println("Main hand sickle");
+        int entityID = target.getId();
+
+        if (inMain && inOff) {
+
+            if (now - lastHitAt > 40) {
+                attackCount = 1;
+            }
+
+            lastHitAt = now;
+
+            if (entityID == previousEntityId) {
+                attackCount++;
+            } else {
+                attackCount = 1;
+                previousEntityId = entityID;
+            }
+
+            System.out.println("Combo: " + attackCount);
+            if (attackCount == 5) {
+                float dmg = (float)((3.0 * DAMAGE_IRON) * 1.5);
+
+                target.damage((ServerWorld) target.getEntityWorld(), player.getDamageSources().playerAttack(player), dmg);
+                System.out.println("Combo 5 bonus dmg: " + dmg);
+            }
+            else if (attackCount == 6) {
+                float dmg = 2f * 4f * DAMAGE_IRON;
+
+                target.damage((ServerWorld) target.getEntityWorld(), player.getDamageSources().playerAttack(player), dmg);
+                System.out.println("Combo finisher dmg: " + dmg);
+
+                attackCount = 1;
             }
         }
     }
-
-
 }
