44cb5720cb67e1d7ef01a461d4a558362d812ca7
cybersage005-netizen <cybersage005@gmail.com>
2026-02-20 21:11:35 +0530

    Merge remote-tracking branch 'origin/main'

--- src/client/java/net/greenjab/nekomasfixed/mixin/client/AbstractFurnaceScreenMixin.java
+++ src/client/java/net/greenjab/nekomasfixed/mixin/client/AbstractFurnaceScreenMixin.java
@@ -1,12 +1,9 @@
-package net.greenjab.nekomasfixed.mixin;
+package net.greenjab.nekomasfixed.mixin.client;
 
 import net.minecraft.client.gui.screen.ingame.AbstractFurnaceScreen;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
