acf4bea11c2278898b3ed3b829a8db171836aedb
cybersage005-netizen <cybersage005@gmail.com>
2026-02-20 22:22:30 +0530

    Merge remote-tracking branch 'origin/main'

--- src/client/java/net/greenjab/nekomasfixed/mixin/client/AbstractFurnaceScreenMixin.java
+++ src/client/java/net/greenjab/nekomasfixed/mixin/client/AbstractFurnaceScreenMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractFurnaceScreenHandler.class)
-public class AbstractFurnaceScreenHandlerMixin {
+public class AbstractFurnaceScreenMixin {
 
     @Inject(method = "populateRecipeFinder", at = @At("HEAD"), cancellable = true)
     private void preventRecipePopulation(RecipeFinder finder, CallbackInfo ci) {
