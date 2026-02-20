3c273d98ce6cd9ebe92cd1ab0053ba9f9774ab6f
cybersage005-netizen <cybersage005@gmail.com>
2026-02-20 22:27:18 +0530

    Merge remote-tracking branch 'origin/main'

--- src/client/java/net/greenjab/nekomasfixed/mixin/client/AbstractFurnaceScreenMixin.java
+++ src/client/java/net/greenjab/nekomasfixed/mixin/client/AbstractFurnaceScreenMixin.java
@@ -1,22 +1,28 @@
 package net.greenjab.nekomasfixed.mixin.client;
 
-import net.minecraft.recipe.RecipeFinder;
+import net.minecraft.client.gui.screen.ingame.AbstractFurnaceScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AbstractFurnaceScreenHandler.class)
+@Mixin(AbstractFurnaceScreen.class)
 public class AbstractFurnaceScreenMixin {
 
-    @Inject(method = "populateRecipeFinder", at = @At("HEAD"), cancellable = true)
-    private void preventRecipePopulation(RecipeFinder finder, CallbackInfo ci) {
-        AbstractFurnaceScreenHandler handler = (AbstractFurnaceScreenHandler) (Object) this;
+
+    private RecipeBookWidget recipeBook;
+
+    @Inject(method = "init", at = @At("RETURN"))
+    private void removeRecipeBook(CallbackInfo ci) {
+        AbstractFurnaceScreen<?> screen = (AbstractFurnaceScreen<?>) (Object) this;
+        AbstractFurnaceScreenHandler handler = screen.getScreenHandler();
 
         // Check if this is your kiln
         if (handler.getClass().getName().contains("KilnScreenHandler")) {
-            ci.cancel(); // This completely prevents recipe population
+            this.recipeBook = null;
         }
     }
 }
