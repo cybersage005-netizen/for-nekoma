8b433860c92ffa5cfa87d3a15eaaf90a6e0f3a32
cybersage005-netizen <cybersage005@gmail.com>
2026-02-21 17:36:26 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/block/HoneyCauldronBlock.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/HoneyCauldronBlock.java
@@ -1,21 +1,14 @@
 package net.greenjab.nekomasfixed.registry.block;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.AbstractCauldronBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.block.cauldron.CauldronBehavior;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -28,36 +21,23 @@ public class HoneyCauldronBlock extends AbstractCauldronBlock {
     private static final CauldronBehavior.CauldronBehaviorMap HONEY_BEHAVIOR =
             CauldronBehavior.createMap("honey");
 
-    @Override
-    public MapCodec<? extends AbstractCauldronBlock> getCodec() {
-        return CODEC;
-    }
-
-    public HoneyCauldronBlock(Settings settings) {
-        super(settings, HONEY_BEHAVIOR);
-        registerBehaviors();
-    }
-
-    public void registerBehaviors() {
+    static {
         Map<Item, CauldronBehavior> map = HONEY_BEHAVIOR.map();
 
-        map.put(Items.HONEY_BOTTLE, (state, world, pos, player, hand, stack) -> {
-            if (!world.isClient()) {
-                player.setStackInHand(hand, new ItemStack(Items.GLASS_BOTTLE));
-                world.setBlockState(pos, this.getDefaultState());
-                world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_EMPTY,
-                        SoundCategory.BLOCKS, 1.0F, 1.0F);
-            }
-            return ActionResult.SUCCESS;
-        });
-
-
+        // Glass bottle takes honey (decreases level)
         map.put(Items.GLASS_BOTTLE, (state, world, pos, player, hand, stack) -> {
             if (!world.isClient()) {
+                int level = state.get(LeveledCauldronBlock.LEVEL);
                 player.setStackInHand(hand, new ItemStack(Items.HONEY_BOTTLE));
-                if (shouldDecrementLevel()) {
+
+                if (level > 1) {
+                    // Decrease level
+                    world.setBlockState(pos, state.with(LeveledCauldronBlock.LEVEL, level - 1));
+                } else {
+                    // Empty cauldron
                     world.setBlockState(pos, Blocks.CAULDRON.getDefaultState());
                 }
+
                 world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_FILL,
                         SoundCategory.BLOCKS, 1.0F, 1.0F);
             }
@@ -65,37 +45,36 @@ public class HoneyCauldronBlock extends AbstractCauldronBlock {
         });
     }
 
+    public HoneyCauldronBlock(Settings settings) {
+        super(settings, HONEY_BEHAVIOR);
+        this.setDefaultState(this.stateManager.getDefaultState()
+                .with(LeveledCauldronBlock.LEVEL, 3)); // Start full
+    }
+
+    @Override
+    protected MapCodec<? extends AbstractCauldronBlock> getCodec() {
+        return null;
+    }
+
     @Override
     protected double getFluidHeight(BlockState state) {
-        return 0.9375;
+        return (6.0 + (double)state.get(LeveledCauldronBlock.LEVEL) * 3.0) / 16.0;
     }
 
     @Override
     public boolean isFull(BlockState state) {
-        return true;
-    }
-
-
-    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        if (entity instanceof LivingEntity living && !world.isClient()) {
-            living.addStatusEffect(new StatusEffectInstance(
-                    StatusEffects.SLOWNESS, 100, 1));
-            living.addStatusEffect(new StatusEffectInstance(
-                    StatusEffects.JUMP_BOOST, 100, 0));
-        }
-    }
-
-    @Override
-    protected boolean canBeFilledByDripstone(Fluid fluid) {
-        return fluid == Fluids.WATER;
+        return state.get(LeveledCauldronBlock.LEVEL) == 3;
     }
 
 
     protected int getComparatorOutput(BlockState state, World world, BlockPos pos) {
-        return 3;
+        return state.get(LeveledCauldronBlock.LEVEL);
     }
 
-    private boolean shouldDecrementLevel() {
-        return true;
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(LeveledCauldronBlock.LEVEL);
     }
+
+    // ... rest of your code
 }
