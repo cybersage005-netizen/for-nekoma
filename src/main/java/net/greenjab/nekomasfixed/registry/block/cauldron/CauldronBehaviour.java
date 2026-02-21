7be0ab7b94fa415228f8a0add74214d3752809e7
cybersage005-netizen <cybersage005@gmail.com>
2026-02-21 12:57:32 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/registry/block/cauldron/CauldronBehaviour.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/cauldron/CauldronBehaviour.java
@@ -1,7 +1,6 @@
 package net.greenjab.nekomasfixed.registry.block.cauldron;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.component.DataComponentTypes;
@@ -9,8 +8,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
@@ -26,7 +23,7 @@ public class CauldronBehaviour {
 
         Map<Item, CauldronBehavior> waterMap = CauldronBehavior.WATER_CAULDRON_BEHAVIOR.map();
 
-
+        System.out.println("Start Registering the Items");
         addWoolBehavior(waterMap, Items.WHITE_WOOL);
         addWoolBehavior(waterMap, Items.ORANGE_WOOL);
         addWoolBehavior(waterMap, Items.MAGENTA_WOOL);
@@ -43,6 +40,7 @@ public class CauldronBehaviour {
         addWoolBehavior(waterMap, Items.GREEN_WOOL);
         addWoolBehavior(waterMap, Items.RED_WOOL);
         addWoolBehavior(waterMap, Items.BLACK_WOOL);
+        System.out.println("Done Registering the Items");
     }
 
     private static void addWoolBehavior(Map<Item, CauldronBehavior> map, Item woolItem) {
@@ -51,13 +49,13 @@ public class CauldronBehaviour {
 
     private static ActionResult cleanWool(BlockState state, World world, BlockPos pos,
                                           PlayerEntity player, Hand hand, ItemStack stack) {
-
+        System.out.println("Action with " + stack + " done on cauldron");
         if (!stack.contains(DataComponentTypes.DYED_COLOR)) {
             return ActionResult.PASS_TO_DEFAULT_BLOCK_ACTION;
         }
 
         if (!world.isClient()) {
-            ItemStack itemStack = stack.copyComponentsToNewStack(Blocks.WHITE_WOOL, 1);
+            stack.remove(DataComponentTypes.DYED_COLOR);
             LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
             world.playSound(null, pos, SoundEvents.ITEM_BUCKET_EMPTY,
                     SoundCategory.BLOCKS, 1.0F, 1.0F);
